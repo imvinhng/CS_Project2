eada3ed64c1f6eafac288a463e0fecef26681af1
imvinhng <63667978+imvinhng@users.noreply.github.com>
2020-05-18 15:39:06 -0700

    Add files via upload

---
+++ List.java
@@ -0,0 +1,31 @@
+/*
+ * ==========================================
+ * CS211, Spring 2020, 5/10
+ * Vinh T. Nguyen , Jae Choi, Alexander Larsen, Sean Micheal
+ * Team Project #2 - Chap 16, page 1027 Programming Project #3
+ * Create an abstract AbstractList superclass that is extended by 
+ * both ArrayList and LinkedList, which factor out the common 
+ * methods/functionality of the two classes
+ * 
+ * 
+ * 
+ * ==========================================
+ */
+
+// Generic interface for a List of E objects.
+// Change this interface into an abstract class
+// which is then extended by both ArrayList and LinkedList
+
+public interface List<E> extends Iterable<E> {  
+    public int size();
+    public E get(int index);
+    public int indexOf(E value);
+    public boolean isEmpty();
+    public boolean contains(E value);
+    public void add(E value);
+    public void add(int index, E value);
+    public void addAll(List<E> other);
+    public void remove(int index);
+    public void set(int index, E value);
+    public void clear();
+}
---
+++ ListClient3.java
@@ -0,0 +1,38 @@
+/*
+ * ==========================================
+ * CS211, Spring 2020, 5/10
+ * Vinh T. Nguyen , Jae Choi, Alexander Larsen, Sean Micheal
+ * Team Project #2 - Chap 16, page 1027 Programming Project #3
+ * Create an abstract AbstractList superclass that is extended by 
+ * both ArrayList and LinkedList, which factor out the common 
+ * methods/functionality of the two classes
+ * 
+ * 
+ * 
+ * ==========================================
+ */
+
+//compiler directive to eliminate superfluous warnings.
+@SuppressWarnings("unchecked")
+
+public class ListClient3 {
+    public static void main(String[] args) {
+        ArrayList<Integer> list1 = new ArrayList<Integer>();
+        processList(list1);
+        System.out.println();
+
+        LinkedList<Integer> list2 = new LinkedList<Integer>();
+        processList(list2);
+    }
+
+    public static void processList(List list) {
+        list.add(42);
+        list.add(18);
+        list.add(27);
+        list.add(93);
+        System.out.println(list);
+        list.remove(2);
+        list.remove(0);
+        System.out.println(list);
+    }
+}
