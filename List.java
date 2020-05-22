8bd7701529c00dd566f310b524b2687b194d56dc
alarsen123 <63680257+alarsen123@users.noreply.github.com>
2020-05-22 16:45:24 -0700

    Add files via upload
    Fixed typo in test client's remove test, made list size smaller so the changes are more noticeable

--- List.java
+++ List.java
@@ -1,18 +1,13 @@
 /*
- * ==========================================
- * CS211, Spring 2020, 5/10
-<<<<<<< HEAD
- * Vinh T. Nguyen , Jae Choi, Alexander Larsen, Sean Michael
-=======
- * Vinh Nguyen , Jae Choi, Alexander Larsen, Sean Micheal
->>>>>>> 9c47a47e5a727a8402c527077baf45bc646be87c
- * Team Project #2 - Chap 16, page 1027 Programming Project #3
- * Generic interface for a List of E objects.
- * Change this interface into an abstract class
- * which is then extended by both ArrayList and LinkedList
- * ==========================================
- */
-public interface List<E> extends Iterable<E> {  
+*  CS211, Spring 2020
+*  Jae Choi, Alex Larsen, Sean Michael, Vinh Nguyen
+*  5/18/2020
+*  Team Project 2
+*/
+
+// Generic interface for a List of E objects.
+
+public interface List<E> extends Iterable<E> {
     public int size();
     public E get(int index);
     public int indexOf(E value);
